38896bb82a95c5da8c4bd5433bf781b034acc961
YoannMnts <yoann.montesinos@gmail.com>
2025-11-19 21:57:26 +0100

    l'ui de debug la

---
+++ GestionProdUnity/Assets/Scripts/Debug/UI.cs
@@ -0,0 +1,24 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class UI : MonoBehaviour
+{
+    [SerializeField]
+    private InputActionAsset inputActionsAsset;
+    [SerializeField]
+    private CanvasGroup debugCanvasGroup;
+    private InputAction menuAction;
+
+    private void Start()
+    {
+        menuAction = inputActionsAsset.FindActionMap("Default").FindAction("DebugMenu");
+    }
+
+    private void FixedUpdate()
+    {
+        var isDebugMenu = (int)menuAction.ReadValue<float>() == 1;
+        Debug.Log(isDebugMenu);
+        debugCanvasGroup.alpha = isDebugMenu ? 1 : 0;
+    }
+}
