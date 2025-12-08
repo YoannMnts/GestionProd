75236fad1897341cccf6aac9b58fa89ec4d562ff
Jules Petit Faucher <jules.petitfaucher@gmail.com>
2025-12-08 20:48:40 +0100

    FuelCrafting en mode hassoul et pas propre

---
+++ GestionProdUnity/Assets/GestionFolder/Scenes/Group3/Scripts/EnumObject.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace Scripts
+{
+    public enum TypeOfObject
+    {
+        BlueFlower, 
+        RedFlower, 
+        GreenFlower,
+        Chemical,
+        Tools,
+    };
+}
+
+
--- GestionProdUnity/Assets/GestionFolder/Scenes/Group3/Scripts/InventoryHassoul.cs
+++ GestionProdUnity/Assets/GestionFolder/Scenes/Group3/Scripts/InventoryHassoul.cs
@@ -1,13 +1,33 @@
+using JetBrains.Annotations;
 using UnityEngine;
 
 public class InventoryHassoul : MonoBehaviour
 {
-    private int NbGrabbed = 0;
-
-    // Update is called once per frame
-    public void ObjectGrabbed()
+    public int BlueFlower { get; private set; }
+    public int RedFlower { get; private set; }
+    public int GreenFlower { get; private set; }
+    public int Chemical { get; private set; }
+    public int Tools { get; private set; }
+    public int Fuel { get; private set; }
+    
+    
+    public void ObjectGrabbed(GameObject GrabbedObject)
     {
-        NbGrabbed++;
-        Debug.Log(NbGrabbed);
+        
+        string objectname = GrabbedObject.name;
+        if (objectname == "BlueFlower")
+            BlueFlower++;
+        else if (objectname == "RedFlower")
+            RedFlower++;
+        else if (objectname == "GreenFlower")
+            GreenFlower++;
+        else if (objectname == "Chemical")
+            Chemical++;
+        else if (objectname == "Tools")
+            Tools++;
+        else if (objectname == "Fuel")
+            Fuel++;
+
+        Debug.Log(objectname);
     }
 }
